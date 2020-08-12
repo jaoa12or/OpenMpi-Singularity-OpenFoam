9a09c36aebb66c4744043a1c61ab0629a0e2b0dc
alejandro orobio <jhon.orobio@correounivalle.edu.co>
2020-08-12 12:02:54 -0500

    feat: instructions to get ready

--- client.sh
+++ client.sh
@@ -3,3 +3,6 @@
 sudo yum install nfs-utils nfs4-acl-tools -y
 sudo systemctl start nfs-server.service
 sudo systemctl enable nfs-server.service
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+sudo yum update
+sudo yum install htop -y
